1d99a46cdced00c04a2f536fc374f5cc5b1b1c51
Akshay K.a <106895549+akshay-k-a-dev@users.noreply.github.com>
2026-08-22 20:54:06 +0530

    Revert settings.gradle.kts to version from 223383e33776f88595535a32d8cf600f847d4c95

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,16 +12,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/akshay-k-a-dev/NewPipeExtractor")
-            credentials {
-                username = providers.gradleProperty("gpr.user")
-                    .orElse(providers.environmentVariable("GPR_USER")).get()
-                password = providers.gradleProperty("gpr.token")
-                    .orElse(providers.environmentVariable("GPR_KEY")).get()
-            }
-        }
     }
 }
 
